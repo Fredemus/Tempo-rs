0305b1cb7c591834793d7f8ad814198a8a8fe302
Fredemus <frederik.halkjaer1@gmail.com>
2019-10-07 13:39:38 +0200

    Setting up framework for tempo detection

---
+++ Tempo/src/main.rs
@@ -0,0 +1,54 @@
+/// This project is meant for opening a wave file and calculating its tempo. Meant as a prototype
+
+struct SoundFile {
+    /// Sound data
+    data: Vec<f32>,
+    //the name of the file that was read into SoundFile
+    file_name: String,
+    fs: usize
+}
+#[allow(dead_code)]
+impl SoundFile {
+    fn load_sound(&mut self, file: String) {
+        
+    }
+    fn search_for_file(&self) {
+
+    }
+    fn generate_file(&self) {
+
+    }
+    fn bpm_from_rhythm(&self, file: &mut Analysis) {
+        let mut transientsum = 0;
+        for i in 0..file.rhythm.len() {
+            if file.rhythm[i] != 0. {
+                transientsum += 1;
+            }
+        }
+        // average transients per second * 60 gives us our bpm
+        file.tempo = (transientsum / file.rhythm.len()) as f32 * self.fs as f32 * 60.;
+    }
+
+}
+impl Default for SoundFile {
+    fn default() -> SoundFile {
+        SoundFile { 
+            data: vec![0.],
+            file_name: format!(""),
+            fs: 44100,
+        }
+    }
+}
+/// contains the data that controls the lightshow. Found on background of SoundFile
+struct Analysis {
+    /// tempo in beats per minute
+    tempo: f32,
+    /// Contains every time a transient is detected. Same time format as the SoundFile
+    rhythm: Vec<f32>,
+}
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
