3c353f7a2f981c77b87f4127e9cbbd074f92c413
Fredemus <frederik.halkjaer1@gmail.com>
2019-10-07 13:49:37 +0200

    Reading of wave files

--- Tempo/src/main.rs
+++ Tempo/src/main.rs
@@ -1,22 +1,26 @@
 /// This project is meant for opening a wave file and calculating its tempo. Meant as a prototype
 
+// hound is a wav file reading library
+extern crate hound;
+
 struct SoundFile {
-    /// Sound data
-    data: Vec<f32>,
+    /// Sound samples
+    samples: Vec<f32>,
     //the name of the file that was read into SoundFile
     file_name: String,
-    fs: usize
+    fs: usize,
 }
 #[allow(dead_code)]
 impl SoundFile {
-    fn load_sound(&mut self, file: String) {
-        
+    fn load_sound(&mut self, path: String) {
+        let mut reader = hound::WavReader::open(path).unwrap();
+        self.samples = reader.samples().collect::<Result<Vec<_>, _>>().unwrap();
     }
     fn search_for_file(&self) {
 
     }
-    fn generate_file(&self) {
-
+    fn generate_analysis_file(&self) {
+        
     }
     fn bpm_from_rhythm(&self, file: &mut Analysis) {
         let mut transientsum = 0;
@@ -33,13 +37,13 @@ impl SoundFile {
 impl Default for SoundFile {
     fn default() -> SoundFile {
         SoundFile { 
-            data: vec![0.],
+            samples: vec![0.],
             file_name: format!(""),
             fs: 44100,
         }
     }
 }
-/// contains the data that controls the lightshow. Found on background of SoundFile
+/// contains the samples that controls the lightshow. Found on background of SoundFile
 struct Analysis {
     /// tempo in beats per minute
     tempo: f32,
